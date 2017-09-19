251c91870ea23d6751dee3505405c56b81d0a14d
Matthew Darling <matthew.darling@kirasystems.com>
2017-09-19 17:22:46 -0300

    Configure doo library for deployment to Clojars

--- library/project.clj
+++ library/project.clj
@@ -7,7 +7,10 @@
   :scm {:name "git"
         :url "https://github.com/bensu/doo"}
 
-  :deploy-repositories [["clojars" {:creds :gpg}]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :sign-releases false
+                              :username :env
+                              :password :env}]]
 
   :test-paths ["test/clj"]
 
@@ -23,4 +26,13 @@
 
   :profiles
   {:dev {:source-paths ["src/clj" "test/clj" "../example/src" "../example/test"]
-         :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]}})
+         :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]]}}
+
+  ;;; Don't sign the tag, and don't deploy
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
